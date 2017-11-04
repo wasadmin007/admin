e731fa6e9c6c0a069cd45c2f5d62dabd2072f7e9
Praveen Jeendru <wasadmin007@gmail.com>
2017-11-04 16:58:14 -0400

    Admin Config Back Up File

---
+++ admin_config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://jindru.com/www/admin/');
+define('HTTP_CATALOG', 'http://jindru.com/www');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://jindru.com/www/admin/');
+define('HTTPS_CATALOG', 'http://jindru.com/www');
+
+// DIR
+define('DIR_APPLICATION', '/home/wasadmin/public_html/www/admin/');
+define('DIR_SYSTEM', '/home/wasadmin/public_html/www/system/');
+define('DIR_DATABASE', '/home/wasadmin/public_html/www/system/database/');
+define('DIR_LANGUAGE', '/home/wasadmin/public_html/www/admin/language/');
+define('DIR_TEMPLATE', '/home/wasadmin/public_html/www/admin/view/template/');
+define('DIR_CONFIG', '/home/wasadmin/public_html/www/system/config/');
+define('DIR_IMAGE', '/home/wasadmin/public_html/www/image/');
+define('DIR_CACHE', '/home/wasadmin/public_html/www/system/cache/');
+define('DIR_DOWNLOAD', '/home/wasadmin/public_html/www/download/');
+define('DIR_LOGS', '/home/wasadmin/public_html/www/system/logs/');
+define('DIR_CATALOG', '/home/wasadmin/public_html/www/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'wasadmin_ocdev');
+define('DB_PASSWORD', 'dP0X!6.S5c');
+define('DB_DATABASE', 'wasadmin_ocdev');
+define('DB_PREFIX', 'oc_');
+
+?>
